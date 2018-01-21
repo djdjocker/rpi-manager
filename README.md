9648ad7d6b80b5a6a53607d8ae2ae7f97df36718
djdjocker <xavier@audenaert.fr>
2018-01-21 12:16:17 +0100

    install proc

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # rpi-manager
+
+Create Node Web server And Python Gui using startx.
+
+The Node server is used to manage the RF modules nearby
+
+## Getting Started
+
+### Installing
+
+Run install script to get correct node/npm vertion.
+
+```
+sudo ./install.sh
+```
+
+The Script will reboot rpi
+
+Run npm install to get node dependences and compile RF communicator
+
+```
+npm start
+```
+
+### Run the program
+
+```
+startx
+```
---
+++ install.sh
@@ -0,0 +1,18 @@
+#Install startx and python deps
+apt-get install -y xinit python-pyqt5 python-pyqt5.qtwebkit
+
+#Install last node js
+wget https://nodejs.org/dist/v9.3.0/node-v9.3.0-linux-armv6l.tar.xz
+xz -d node-v9.3.0-linux-armv6l.tar.xz
+tar -xf node-v9.3.0-linux-armv6l.tar
+
+cd node-v9.3.0-linux-armv6l/
+sudo rsync -av --exclude="*.md" --exclude="LICENSE" /usr/local/
+# ou 
+#mv node-v9.3.0-linux-armv6l node
+#sed -i "$ a \
+#if [ -d \"\$HOME/node\" ] ; then \
+#    PATH=\"\$HOME/node/bin:\$PATH\" \
+#fi" .profile
+
+sudo reboot
